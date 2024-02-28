a238ed9eeb74ff833f434115b1999186302e8efd
krishnaLilhare <krishnalilhare671@gmail.com>
2024-02-28 15:21:18 +0530

    chnage new file

--- HotelMicroservice/src/main/java/com/krishna/hotel/exception/GlobalExceptionHandler.java
+++ HotelMicroservice/src/main/java/com/krishna/hotel/exception/GlobalExceptionHandler.java
@@ -1,4 +1,4 @@
-package com.krishna.rating.exception;
+package com.krishna.hotel.exception;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
